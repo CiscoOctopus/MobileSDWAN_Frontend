12b31988239eeb8958673566e31e91fd3fa50167
Jiaming Li <jiaminli@cisco.com>
2020-08-19 18:23:46 +0800

    Optimized the frontend & fixed the add cache issue

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -1,177 +1,218 @@
-import React, { useState } from "react";
-import { IonContent, 
-  IonHeader, IonPage, IonTitle, 
-  IonToolbar, IonButton, IonIcon, IonModal, IonButtons,
-  IonList, IonItem, IonLabel, IonInput, IonText, IonSelect, IonSelectOption, IonActionSheet,
+import React, {useState} from "react";
+import {
+    IonContent,
+    IonHeader, IonPage, IonTitle,
+    IonToolbar, IonButton, IonIcon, IonModal, IonButtons,
+    IonList, IonItem, IonLabel, IonInput, IonText, IonSelect, IonSelectOption, IonActionSheet, IonCard, IonCardContent,
+    IonBadge, IonFabButton, IonFab,
 } from '@ionic/react';
+import {pin, wifi, wine, warning, walk, add, checkmark} from 'ionicons/icons';
 
 import './Admin.css';
 
-import {personAddOutline, closeOutline, trash, close} from 'ionicons/icons';
-import { useVpnList, Vpn } from '../hooks/useVpnList';
-import { useForm } from "react-hook-form";
+import {personAddOutline, closeOutline, trash, close, logoApple, server} from 'ionicons/icons';
+import {useVpnList, Vpn} from '../hooks/useVpnList';
+import {useForm} from "react-hook-form";
 
 let initialValues = {
-  username: '',
-  company_name: '',
-  password: "",
-  confirmPassword: ""
+    username: '',
+    company_name: '',
+    password: "",
+    confirmPassword: ""
 };
 
 const Tab1: React.FC = () => {
-  const [showModal, setShowModal] = useState(false);
+    const [showModal, setShowModal] = useState(false);
 
-  const { vpnList, deleteVpn, addUser } = useVpnList();
-  const [vpnToDelete, setVpnToDelete] = useState<Vpn>();
+    let {vpnList, deleteVpn, addUser} = useVpnList();
+    const [vpnToDelete, setVpnToDelete] = useState<Vpn>();
 
-  const { handleSubmit, errors} = useForm({
-    defaultValues: { ...initialValues },
-    mode: "onChange"
-  });
 
-  const onSubmit = (data: any) => {
-    // setData(data);
-    // if (initialValues.password !== initialValues.confirmPassword) {
-    //   // control.set. = {message : 'Password mismatch!'}
-    // } else {
-    // }
-    let vpn = {
-      username: initialValues.username,
-      password: initialValues.password,
-      company_name: initialValues.company_name
+
+    const {handleSubmit, errors} = useForm({
+        defaultValues: {...initialValues},
+        mode: "onChange"
+    });
+
+    const companies_color: any = {
+        "apple":'#C0C0C0',
+        "ibm":'#0CA9EA',
+
     };
-    addUser(vpn)
-    setShowModal(false)
-  };
 
-  const showError = (_fieldName: string) => {
-    let error = (errors as any)[_fieldName];
-    return error ? (
-      <div style={{ color: "red", fontWeight: "bold" }}>
-        {error.message || "Field Is Required"}
-      </div>
-    ) : null;
-  };
+    const companies_logo: any = {
+        "apple":logoApple,
+        "ibm":server,
 
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Users</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Users</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <IonButton color="primary" onClick={() => setShowModal(true)}>
-          <IonIcon icon={personAddOutline}></IonIcon>
-        </IonButton>
+    };
 
-        <table>
-          <thead>
-            <tr>
-              <th>User</th>
-              <th>Company</th>
-            </tr>
-          </thead>
-          <tbody>
-          {vpnList.map(e=>
-            <tr key={e.username} onClick={() => setVpnToDelete(e)}>
-              <td>{e.username}</td>
-              <td>{e.company_name}</td>
-            </tr>
-          )}
-          </tbody>
-        </table>
+    const companies_badge: any = {
+        "apple":"light",
+        "ibm":"primary",
 
-        <IonActionSheet
-          isOpen={!!vpnToDelete}
-          buttons={[{
-            text: 'Delete',
-            role: 'destructive',
-            icon: trash,
-            handler: () => {
-              if (vpnToDelete) {
-                deleteVpn(vpnToDelete);
-                setVpnToDelete(undefined);
-              }
-            }
-          }, {
-            text: 'Cancel',
-            icon: close,
-            role: 'cancel'
-          }]}
-          onDidDismiss={() => setVpnToDelete(undefined)}
-        />
-        <IonModal isOpen={showModal} cssClass='my-custom-class'>
-          <IonHeader translucent>
-            <IonToolbar>
-              <IonTitle>Add New User</IonTitle>
-              <IonButtons slot="end">
-                <IonButton onClick={() => setShowModal(false)}>
-                  <IonIcon icon={closeOutline}></IonIcon>
-                </IonButton>
-              </IonButtons>
-            </IonToolbar>
-          </IonHeader>
-          <IonContent>
-            <form onSubmit={handleSubmit(onSubmit)}>
-              <IonList lines="full" class="ion-no-margin ion-no-padding">
-                <IonItem>
-                  <IonLabel position="floating">Company</IonLabel>
-                  <IonSelect value={initialValues.company_name}
-                    placeholder="Select One" 
-                    name="company_name"
-                    onIonChange={e=>initialValues.company_name=e.detail.value}
-                    >
-                    <IonSelectOption value="apple">APPLE</IonSelectOption>
-                    <IonSelectOption value="ibm">IBM</IonSelectOption>
-                  </IonSelect>
-                </IonItem>
-                {showError("company_name")}
+    };
 
-                <IonItem>
-                  <IonLabel position="stacked">User Name<IonText color="danger">*</IonText></IonLabel>
-                  <IonInput 
-                    value={initialValues.username}
-                    name="username" 
-                    required
-                    type="text"
-                    onIonChange={(e:any)=>initialValues.username=e.detail.value}
-                  ></IonInput>
-                </IonItem>
-                {showError("username")}
 
-                <IonItem>
-                  <IonLabel position="stacked">Password<IonText color="danger">*</IonText></IonLabel>
-                  <IonInput
-                    value={initialValues.password}
-                    required type="password"
-                    onIonChange={(e:any)=>initialValues.password=e.detail.value}
-                  ></IonInput>
-                </IonItem>
 
-                <IonItem>
-                  <IonLabel position="stacked">Confirm Password<IonText color="danger">*</IonText></IonLabel>
-                  <IonInput
-                    value={initialValues.confirmPassword}
-                    required
-                    type="password"
-                    onIonChange={(e:any)=>initialValues.confirmPassword=e.detail.value}
-                    ></IonInput>
-                </IonItem>
-                {showError("confirmPassword")}
-              </IonList>
-              <IonButton type="submit">Add</IonButton>
-            </form>
-          </IonContent>
-        </IonModal>
-      </IonContent>
-    </IonPage>
-  );
+    const formatData = (data:any)=>{
+    }
+
+
+    const onSubmit = (data: any) => {
+        // setData(data);
+        // if (initialValues.password !== initialValues.confirmPassword) {
+        //   // control.set. = {message : 'Password mismatch!'}
+        // } else {
+        // }
+        let vpn = {
+            username: initialValues.username,
+            password: initialValues.password,
+            company_name: initialValues.company_name
+        };
+        addUser(vpn);
+        let new_vpn: Vpn ={
+            username: initialValues.username,
+            company_name: initialValues.company_name
+        };
+        vpnList.push(new_vpn);
+
+        setShowModal(false)
+    };
+
+    const showError = (_fieldName: string) => {
+        let error = (errors as any)[_fieldName];
+        return error ? (
+            <div style={{color: "red", fontWeight: "bold"}}>
+                {error.message || "Field Is Required"}
+            </div>
+        ) : null;
+    };
+
+    return (
+        <IonPage>
+            <IonHeader>
+                <IonToolbar>
+                    <IonTitle>Users</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+            <IonContent>
+                <IonHeader collapse="condense">
+                    <IonToolbar>
+                        <IonTitle size="large">Users</IonTitle>
+                    </IonToolbar>
+                </IonHeader>
+                {/*<IonButton color="primary" onClick={() => setShowModal(true)}>*/}
+                    {/*<IonIcon icon={personAddOutline}></IonIcon>*/}
+                {/*</IonButton>*/}
+                <IonFab vertical="bottom" horizontal="end" slot="fixed">
+                    <IonFabButton  onClick={() => setShowModal(true)}>
+                        <IonIcon icon={add} />
+                    </IonFabButton>
+                </IonFab>
+
+                <IonList>
+                    {vpnList.map(e =>
+                        <IonItem button onClick={() => setVpnToDelete(e)}>
+                            <IonIcon slot="start" icon={companies_logo[e.company_name]} style={{"color": companies_color[e.company_name]}}></IonIcon>
+                            <IonLabel>
+                                <h2 style={{"display":"inline"}}>{e.username}</h2>
+                            </IonLabel>
+                            <IonBadge color={companies_badge[e.company_name]}>{e.company_name}</IonBadge>
+
+                        </IonItem>
+                    )}
+                </IonList>
+
+                <IonActionSheet
+                    isOpen={!!vpnToDelete}
+                    buttons={[{
+                        text: 'Delete',
+                        role: 'destructive',
+                        icon: trash,
+                        handler: () => {
+                            if (vpnToDelete) {
+                                deleteVpn(vpnToDelete);
+                                setVpnToDelete(undefined);
+                            }
+                        }
+                    }, {
+                        text: 'Cancel',
+                        icon: close,
+                        role: 'cancel'
+                    }]}
+                    onDidDismiss={() => setVpnToDelete(undefined)}
+                />
+                <IonModal isOpen={showModal} cssClass='my-custom-class'>
+                    <IonHeader translucent>
+                        <IonToolbar>
+                            <IonTitle>Add New User</IonTitle>
+                            <IonButtons slot="end">
+                                <IonButton onClick={() => setShowModal(false)}>
+                                    <IonIcon icon={closeOutline}></IonIcon>
+                                </IonButton>
+                            </IonButtons>
+                        </IonToolbar>
+                    </IonHeader>
+                    <IonContent>
+                        <form onSubmit={handleSubmit(onSubmit)}>
+                            <IonList lines="full" class="ion-no-margin ion-no-padding">
+                                <IonItem>
+                                    <IonLabel position="floating">Company</IonLabel>
+                                    <IonSelect value={initialValues.company_name}
+                                               placeholder="Select One"
+                                               name="company_name"
+                                               onIonChange={e => initialValues.company_name = e.detail.value}
+                                    >
+                                        <IonSelectOption value="apple">APPLE</IonSelectOption>
+                                        <IonSelectOption value="ibm">IBM</IonSelectOption>
+                                    </IonSelect>
+                                </IonItem>
+                                {showError("company_name")}
+
+                                <IonItem>
+                                    <IonLabel position="stacked">User Name<IonText color="danger">*</IonText></IonLabel>
+                                    <IonInput
+                                        value={initialValues.username}
+                                        name="username"
+                                        required
+                                        type="text"
+                                        onIonChange={(e: any) => initialValues.username = e.detail.value}
+                                    ></IonInput>
+                                </IonItem>
+                                {showError("username")}
+
+                                <IonItem>
+                                    <IonLabel position="stacked">Password<IonText color="danger">*</IonText></IonLabel>
+                                    <IonInput
+                                        value={initialValues.password}
+                                        required type="password"
+                                        onIonChange={(e: any) => initialValues.password = e.detail.value}
+                                    ></IonInput>
+                                </IonItem>
+
+                                <IonItem>
+                                    <IonLabel position="stacked">Confirm Password<IonText
+                                        color="danger">*</IonText></IonLabel>
+                                    <IonInput
+                                        value={initialValues.confirmPassword}
+                                        required
+                                        type="password"
+                                        onIonChange={(e: any) => initialValues.confirmPassword = e.detail.value}
+                                    ></IonInput>
+                                </IonItem>
+                                {showError("confirmPassword")}
+                            </IonList>
+                            <IonFab vertical="bottom" horizontal="end" slot="fixed">
+                                <IonButton  type="submit" color="success">
+                                    <IonIcon icon={checkmark} />
+                                </IonButton>
+                            </IonFab>
+                        </form>
+                    </IonContent>
+                </IonModal>
+            </IonContent>
+        </IonPage>
+    );
 };
 
 export default Tab1;
